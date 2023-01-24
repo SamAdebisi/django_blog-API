1dcf2bad7ee709170c557bfbfeafb111c5428bab
SamAdebisi <samadebisi12@gmail.com>
2023-01-24 16:11:37 +0100

    posts/models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title 
