bde545a0d66c0ba2727e412e261c807a10ab5ba1
SamAdebisi <samadebisi12@gmail.com>
2023-01-26 17:16:44 +0100

    explicitly set authentication

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -145,7 +145,11 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         # "rest_framework.permissions.AllowAny",
         "rest_framework.permissions.IsAuthenticated",
-    ]
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ],
 }
 
 CORS_ORIGIN_WHITELIST = (
