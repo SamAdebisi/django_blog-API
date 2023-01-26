71d0f7df1c56e820acd58c9bbb51d98e3c87d939
SamAdebisi <samadebisi12@gmail.com>
2023-01-26 18:39:40 +0100

    registration url

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path("api/v1/", include("posts.urls")),
     path("api-auth/", include("rest_framework.urls")),
     path("api/v1/dj-rest-auth/", include("dj_rest_auth.urls")),
+    path("api/v1/dj-rest-auth/registration/",
+         include("dj_rest_auth.registration.urls")),
 ]
