230cd55e3896dcecc191f9b5d43a91354340bae7
SamAdebisi <samadebisi12@gmail.com>
2023-01-26 17:26:56 +0100

    token authentication

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -148,7 +148,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ],
 }
 
