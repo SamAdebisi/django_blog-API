b659d970375dd92c3bf035b641dc33ed2fb19099
SamAdebisi <samadebisi12@gmail.com>
2023-01-27 19:13:18 +0100

    spectacular url

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -23,4 +24,5 @@ urlpatterns = [
     path("api/v1/dj-rest-auth/", include("dj_rest_auth.urls")),
     path("api/v1/dj-rest-auth/registration/",
          include("dj_rest_auth.registration.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
 ]
