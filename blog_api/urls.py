4cd9f4b8b95a121ebe0018c4f6c4abeafa962ce9
SamAdebisi <samadebisi12@gmail.com>
2023-01-27 19:34:48 +0100

    swagger-ui url

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView, SpectacularRedocView,
+    SpectacularSwaggerView,
 )
 
 urlpatterns = [
@@ -29,5 +30,9 @@ urlpatterns = [
     path("api/schema/",
          SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/redoc/",
-         SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+         SpectacularRedocView.as_view(url_name="schema"),
+         name="redoc"),
+    path("api/schema/swagger-ui/",
+         SpectacularSwaggerView.as_view(url_name="schema"),
+         name="swagger-ui"),
 ]
