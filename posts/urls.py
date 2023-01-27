4b1cdfaad266dc724114be48833d57dfbaac8494
SamAdebisi <samadebisi12@gmail.com>
2023-01-27 16:48:32 +0100

    posts/urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import PostList, PostDetail
+from .views import (
+    PostList, PostDetail,
+    UserList, UserDetail,
+)
 
 urlpatterns = [
-    path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
-    path("", PostList.as_view(), name="post_list"),
+    path("users/", UserList.as_view()),
+    path("users/<int:pk>/", UserDetail.as_view()),
+    path("<int:pk>/", PostDetail.as_view()),
+    path("", PostList.as_view()),
 ]
