6d55ac0581890321d0753f640bca38415e52e695
SamAdebisi <samadebisi12@gmail.com>
2023-01-28 19:07:57 +0100

    DEBUG setup

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG", default=False)
 
 ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
 
