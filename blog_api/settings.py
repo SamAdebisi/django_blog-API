9c0bde25cd17b0bf89b1cf84940e72e0407d1654
SamAdebisi <samadebisi12@gmail.com>
2023-01-28 20:37:02 +0100

    requiements

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles" 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
