1869152e1bb9cbcf4e657a2a3f814e8299de9f69
SamAdebisi <samadebisi12@gmail.com>
2023-01-28 19:33:53 +0100

    allowed_hosts

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", default=False)
 
-ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
